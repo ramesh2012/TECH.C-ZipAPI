4ff066372fe181df3d4c1077c108d3bbd3269f78
matsumoto <matsumoto>
2014-06-21 16:19:17 +0900

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import http.ServerHttpIF;
+
+import org.apache.http.HttpEntity;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("test");
+		
+		String url = 
+				"http://zip.cgis.biz/xml/zip.php?zn=";
+		String accessUrl = url + "1520022";
+		
+		ServerHttpIF httpIF = new ServerHttpIF();
+		HttpEntity entity = 
+				httpIF.httpGet(accessUrl);
+		
+		System.out.println(httpIF.responseText());
+		
 	}
 }
